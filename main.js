fff738e6ce1b4839ee5b25c10cb8a01731c18534
Kactus29 <165764581+Kactus29@users.noreply.github.com>
2025-03-13 00:20:19 +0100

    nb of signals local saved

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-let counter = 0;
+let counter = localStorage.getItem('counter') ? parseInt(localStorage.getItem('counter')) : 0;
 const counterDisplay = document.getElementById('counter');
 const antenne = document.getElementById('antenne');
+
+counterDisplay.textContent = counter;
 
 antenne.addEventListener('click', () => {
     counter++;
     counterDisplay.textContent = counter;
+    localStorage.setItem('counter', counter);
 });
